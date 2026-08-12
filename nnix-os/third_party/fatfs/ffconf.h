1ad46ea3ba39748ae074d7cadfa5c3b40b704d77
nananina <nananina0415@gmail.com>
2026-08-12 14:04:46 +0900

    fix: FatFs 설정 변경, RTC 없음

--- nnix-os/third_party/fatfs/ffconf.h
+++ nnix-os/third_party/fatfs/ffconf.h
@@ -84,7 +84,7 @@
 / Locale and Namespace Configurations
 /---------------------------------------------------------------------------*/
 
-#define FF_CODE_PAGE	932
+#define FF_CODE_PAGE	437
 /* This option specifies the OEM code page to be used on the target system.
 /  Incorrect code page setting can cause a file open failure.
 /
@@ -237,7 +237,7 @@
 /  Note that enabling exFAT discards ANSI C (C89) compatibility. */
 
 
-#define FF_FS_NORTC		0
+#define FF_FS_NORTC		1
 #define FF_NORTC_MON	1
 #define FF_NORTC_MDAY	1
 #define FF_NORTC_YEAR	2022
