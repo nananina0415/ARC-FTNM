275287e40518e7ff46666c01d462686c4715d429
nananina <nananina0415@gmail.com>
2026-08-12 17:27:53 +0900

    fix: 헤더 누락 픽스

--- nnix-os/libc/string.h
+++ nnix-os/libc/string.h
@@ -8,4 +8,10 @@ void* memmove(void* dst, const void* src, size_t n);
 void* memset (void* dst, int c, size_t n);
 int   memcmp (const void* a, const void* b, size_t n);
 
+size_t strlen (const char* s);
+char*  strcpy (char* dst, const char* src);
+char*  strncpy(char* dst, const char* src, size_t n);
+int    strcmp (const char* a, const char* b);
+int    strncmp(const char* a, const char* b, size_t n);
+
 #endif
